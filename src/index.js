68146d2802245f94bc0f568650bcf534bf35b188
Dulce Fabiola <104787408+DulceFabiolaRM@users.noreply.github.com>
2022-09-06 20:28:20 -0500

    hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// import { HooksApp } from './HooksApp';
+// import { CounterApp } from './01-useState/CounterApp';
+// import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { Layout } from './05-useLayoutEffect/Layout';
+// import { Memorize } from './06-memos/Memorize';
+// import { MemoHook } from './06-memos/MemoHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+//import { Padre } from './07-tarea-memo/Padre';
+import { TodoAdd } from './08-useReducer/TodoAdd';
+
+
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+    <TodoAdd />
+  // </React.StrictMode>
+)
